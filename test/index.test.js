3235d9ffe580ff1dcf221a6882e27a990041f72e
Frank Lemanschik <frank@dspeed.eu>
2020-11-22 09:19:52 +0100

    Some improvements for better ESM/CJS interop inside nodejs better rollup support

--- test/index.test.js
+++ test/index.test.js
@@ -1,3 +1,104 @@
+const assert = require('assert');
+const adapterTests = require('@feathersjs/adapter-tests');
+const errors = require('@feathersjs/errors');
+const feathers = require('@feathersjs/feathers');
+const couchbase = require('couchbase');
+
+const couchbaseService = require('../lib');
+const testSuite = adapterTests([
+  '.options',
+  '.events',
+  '._get',
+  '._find',
+  '._create',
+  '._update',
+  '._patch',
+  '._remove',
+  '.get',
+  '.get + $select',
+  '.get + id + query',
+  '.get + NotFound',
+  '.get + id + query id',
+  '.find',
+  '.remove',
+  '.remove + $select',
+  '.remove + id + query',
+  '.remove + multi',
+  '.remove + id + query id',
+  '.update',
+  '.update + $select',
+  '.update + id + query',
+  '.update + NotFound',
+  '.update + id + query id',
+  '.patch',
+  '.patch + $select',
+  '.patch + id + query',
+  '.patch multiple',
+  '.patch multi query',
+  '.patch + NotFound',
+  '.patch + id + query id',
+  '.create',
+  '.create + $select',
+  '.create multi',
+  'internal .find',
+  'internal .get',
+  'internal .create',
+  'internal .update',
+  'internal .patch',
+  'internal .remove',
+  '.find + equal',
+  '.find + equal multiple',
+  '.find + $sort',
+  '.find + $sort + string',
+  '.find + $limit',
+  '.find + $limit 0',
+  '.find + $skip',
+  '.find + $select',
+  '.find + $or',
+  '.find + $in',
+  '.find + $nin',
+  '.find + $lt',
+  '.find + $lte',
+  '.find + $gt',
+  '.find + $gte',
+  '.find + $ne',
+  '.find + $gt + $lt + $sort',
+  '.find + $or nested + $sort',
+  '.find + paginate',
+  '.find + paginate + $limit + $skip',
+  '.find + paginate + $limit 0',
+  '.find + paginate + params'
+]);
+
+describe('Feathers Couchbase Service', () => {
+  const cluster = new couchbase.Cluster('couchbase://localhost', {
+    username: 'Administrator',
+    password: 'test123'
+  });
+  const app = feathers()
+    .use('/people', couchbaseService({
+      cluster,
+      couchbase: {
+        scanConsistency: couchbase.QueryScanConsistency.RequestPlus
+      },
+      name: 'feathers-test',
+      events: ['testing']
+    }));
+    // .use('/people-customid', memory({
+    //   id: 'customid', events
+    // }));
+
+  before(async () => {
+    await cluster.query('DELETE from `feathers-test`');
+  });
+
+  it('exports', () => {
+    assert.ok(typeof couchbaseService === 'function');
+  });
+
+  testSuite(app, errors, 'people', 'id');
+  // testSuite(app, errors, 'people-customid', 'customid');
+});
 /*
 return 0
 
